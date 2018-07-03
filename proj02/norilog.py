e9b289866db1af682b5e34aedfd5d86fcf554967
sky0621 <sky14work@gmail.com>
2018-07-03 19:54:15 +0900

    continue

---
+++ proj02/norilog.py
@@ -0,0 +1,30 @@
+import json
+
+DATA_FILE = 'norilog.json'
+
+def save_data(start, finish, memo, created_at):
+    """記録データを保存します
+    :param start: 乗った駅
+    :type start: str
+    :param finish: 降りた駅
+    :type finish: str
+    :param memo: 乗り降りのメモ
+    :type memo: str
+    :param created_at: 乗り降りの日付
+    :type created_at datetime.datetime
+    :return: None
+    """
+    try:
+        # jsonモジュールでデータベースファイルを開きます。
+        database = json.load(open(DATA_FILE, mode="r", encoding="utf-8"))
+    except FileNotFoundError:
+        database = []
+
+    database.insert(0, {
+        "start": start,
+        "finish": finish,
+        "memo": memo,
+        "created_at": created_at.strftime("%Y-%m-%d %H:%M")
+    })
+
+    json.dump(database, open(DATA_FILE, mode="w", encoding="utf-8"), indent=4, ensure_ascii=False)
